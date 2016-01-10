586428095ea0996954c467b80d96950cfc39d7ec
James Swineson <jamesswineson@gmail.com>
2016-01-10 12:00:16 +0800

    now AST shows correctly

--- test.py
+++ test.py
@@ -14,14 +14,15 @@ def translate_to_c(filename):
     """ Simply use the c_generator module to emit a parsed AST.
     """
     ast = parse_file(filename
+            , clean_code=True, clean_comments=True, clean_macros=False
             , use_cpp=True
             , cpp_path='clang'
             , cpp_args=['-E', '-Wno-macro-redefined', r'-I/Users/james/Copy/Code/Menelaos/menelaos-test-project/testcode3/JStudentManager/JStudentManager', '-I'+os.path.dirname(os.path.abspath(__file__))+r'/pycparser/utils/fake_libc_include', '-nostdinc']
             )
 
     ast.show()
-    generator = c_generator.CGenerator()
-    print(generator.visit(ast))
+    #generator = c_generator.CGenerator()
+    #print(generator.visit(ast))
 
 #------------------------------------------------------------------------------
 if __name__ == "__main__":
